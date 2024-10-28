e86cc80495c2cff286ed97be2eb96d6fa731771b
LolekLiam <63475764+LolekLiam@users.noreply.github.com>
2024-10-28 15:30:28 +0100

    Update bhlnk.sh
    test fix

--- bhlnk.sh
+++ bhlnk.sh
@@ -2,94 +2,103 @@
 dir=$(pwd)
 repM="python3 $dir/bin/strRep.py"
 
+# Function to get file directory
 get_file_dir() {
-	if [[ $1 ]]; then
-		test=$(sudo find "$dir/img_temp/" -type f -name "$1")
-		for i in $test; do
-			echo $i
-		done
-	else 
-		return 0
-	fi
-
+    if [[ $1 ]]; then
+        test=$(sudo find "$dir/img_temp/" -type f -name "$1")
+        for i in $test; do
+            echo $i
+        done
+    else 
+        return 0
+    fi
 }
 
-
+# Function to move files to specified folders
 mvst() {
-	scr_folder=$(dirname $(sudo find -name $1 | sed -e 's,^\./,,' ))
-	if [[ $3 ]]; then
-		scr_folder=$(echo $scr_folder | cut -d'/' -f2-)
-	fi
-	des_folder=$(sudo find -name $2 | sed -e 's,^\./,,')
-	mkdir -p $des_folder/$scr_folder
-	mv $(sudo find -type f -name $1 | sed -e 's,^\./,,' ) $des_folder/$scr_folder
-
+    scr_folder=$(dirname $(sudo find -name $1 | sed -e 's,^\./,,' ))
+    if [[ $3 ]]; then
+        scr_folder=$(echo $scr_folder | cut -d'/' -f2-)
+    fi
+    des_folder=$(sudo find -name $2 | sed -e 's,^\./,,' )
+    mkdir -p $des_folder/$scr_folder
+    mv $(sudo find -type f -name $1 | sed -e 's,^\./,,' ) $des_folder/$scr_folder
 }
 
-jar_util() 
-{
-	cd $dir
+# Function to decompile and recompile .dex files with debug output
+jar_util() {
+    cd $dir
 
-	if [[ ! -d $dir/jar_temp ]]; then
-		mkdir $dir/jar_temp
-	fi
+    if [[ ! -d $dir/jar_temp ]]; then
+        mkdir $dir/jar_temp
+    fi
 
-	bak="java -jar $dir/bin/baksmali.jar d"
-	sma="java -jar $dir/bin/smali-2.5.2.jar a"
+    bak="java -jar $dir/bin/baksmali.jar d"
+    sma="java -jar $dir/bin/smali-2.5.2.jar a"
 
-
-	if [[ $1 == "d" ]]; then
-		echo -ne "====> Patching $2 : "
-		if [[ $(get_file_dir $2 ) ]]; then
-			sudo cp $(get_file_dir $2 ) $dir/jar_temp
-			sudo chown $(whoami) $dir/jar_temp/$2
-			unzip $dir/jar_temp/$2 -d $dir/jar_temp/$2.out  >/dev/null 2>&1
-			if [[ -d $dir/jar_temp/"$2.out" ]]; then
-				rm -rf $dir/jar_temp/$2
-				for dex in $(sudo find $dir/jar_temp/"$2.out" -maxdepth 1 -name "*dex" ); do
-						if [[ $4 ]]; then
-							if [[ "$dex" != *"$4"* && "$dex" != *"$5"* ]]; then
-								$bak $dex -o "$dex.out"
-								[[ -d "$dex.out" ]] && rm -rf $dex
-							fi
-						else
-							$bak $dex -o "$dex.out"
-							[[ -d "$dex.out" ]] && rm -rf $dex		
-						fi
-
-				done
-			fi
-		fi
-	else 
-		if [[ $1 == "a" ]]; then 
-			if [[ -d $dir/jar_temp/$2.out ]]; then
-				cd $dir/jar_temp/$2.out
-				for fld in $(sudo find -maxdepth 1 -name "*.out" ); do
-					if [[ $4 ]]; then
-						if [[ "$fld" != *"$4"* && "$fld" != *"$5"* ]]; then
-							$sma $fld -o $(echo ${fld//.out}) --api 34
-							[[ -f $(echo ${fld//.out}) ]] && rm -rf $fld
-						fi
-					else 
-						$sma $fld -o $(echo ${fld//.out}) --api 34
-						[[ -f $(echo ${fld//.out}) ]] && rm -rf $fld	
-					fi
-				done
-				7za a -tzip -mx=0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/. >/dev/null 2>&1
-				#zip -r -j -0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/.
-				$zipalign_sa -p -v 4 $dir/jar_temp/$2_notal $dir/jar_temp/$2  >/dev/null 2>&1
-				if [[ -f $dir/jar_temp/$2 ]]; then
-					rm -rf $dir/jar_temp/$2.out $dir/jar_temp/$2_notal 
-					sudo cp -rf $dir/jar_temp/$2 $(get_file_dir $2) 
-					echo "Succes"
-				else
-					echo "Fail"
-				fi
-			fi
-		fi
-	fi
+    if [[ $1 == "d" ]]; then
+        echo -ne "====> Patching $2 : "
+        
+        # Checking if the file exists
+        if [[ $(get_file_dir $2 ) ]]; then
+            sudo cp $(get_file_dir $2 ) $dir/jar_temp
+            sudo chown $(whoami) $dir/jar_temp/$2
+            unzip $dir/jar_temp/$2 -d $dir/jar_temp/$2.out >/dev/null 2>&1
+            
+            # Confirm directory creation
+            if [[ -d $dir/jar_temp/"$2.out" ]]; then
+                rm -rf $dir/jar_temp/$2
+                for dex in $(sudo find $dir/jar_temp/"$2.out" -maxdepth 1 -name "*dex"); do
+                    echo "====> Decompiling $dex with baksmali"
+                    $bak $dex -o "$dex.out"
+                    
+                    # Check if decompilation was successful
+                    if [[ -d "$dex.out" ]]; then
+                        echo "Decompilation successful: $dex.out created"
+                        rm -rf $dex
+                    else
+                        echo "Error: Failed to create $dex.out"
+                    fi
+                done
+            else
+                echo "Error: Failed to create jar_temp/$2.out directory"
+            fi
+        else
+            echo "Error: $2 not found for patching"
+        fi
+    elif [[ $1 == "a" ]]; then
+        if [[ -d $dir/jar_temp/$2.out ]]; then
+            cd $dir/jar_temp/$2.out
+            for fld in $(sudo find -maxdepth 1 -name "*.out"); do
+                echo "====> Recompiling $fld with smali"
+                $sma $fld -o $(echo ${fld//.out}) --api 34
+                
+                # Check if recompilation was successful
+                if [[ -f $(echo ${fld//.out}) ]]; then
+                    echo "Recompilation successful: $(echo ${fld//.out}) created"
+                    rm -rf $fld
+                else
+                    echo "Error: Failed to create $(echo ${fld//.out})"
+                fi
+            done
+            7za a -tzip -mx=0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/. >/dev/null 2>&1
+        else
+            echo "Error: $2.out directory not found for recompilation"
+        fi
+    fi
 }
 
+# Main processing block
+echo "Starting processing..."
+count=$(ls -dq classes* | wc -l)
+mkdir "classes$count.dex.out"
+
+# Verify directory creation for debugging
+if [[ -d "classes$count.dex.out" ]]; then
+    echo "Directory created successfully: classes$count.dex.out"
+else
+    echo "Error: Failed to create classes$count.dex.out"
+fi
 
 repM () {
 	if [[ $4 == "r" ]]; then
